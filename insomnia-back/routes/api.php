53fe3e91469fd0725241ae6884f4d7563d0835b6
davserv <dav.1234rp@gmail.com>
2026-06-27 21:57:33 +0700

    fitur home dan setting account

--- insomnia-back/routes/api.php
+++ insomnia-back/routes/api.php
@@ -1,16 +1,23 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\HomeController;
+use App\Http\Controllers\Api\SettingAccountController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function() {
+    // home
+    Route::get('/home', [HomeController::class, 'index']);
+    // mockSleepLog hanya untuk testing, kalau mau deploy di comment saja codenya
+    Route::post('/home/mock-sleep', [HomeController::class, 'mockSleepLog']);
+
+    // setting account
+    Route::get('/account', [SettingAccountController::class, 'index']);
+    Route::post('/account/update', [SettingAccountController::class, 'update']);
+
     Route::post('/logout', [AuthController::class, 'logout']);
 });
